5a387624297b2983b907363aa097e8b89def398b
fatah-illah <fatahillahera18@gmail.com>
2023-07-22 14:53:19 +0700

    Update initRouter

--- server/paymentServer/initRouter.go
+++ server/paymentServer/initRouter.go
@@ -45,13 +45,5 @@ func InitRouter(controllerMgr *controllers.ControllersManager) *gin.Engine {
 	paymentRoute.PUT("/transaction/update/:id", controllerMgr.PaymentTransactionController.UpdatePaymentTransaction)
 	paymentRoute.DELETE("/transaction/delete/:id", controllerMgr.PaymentTransactionController.DeletePaymentTransaction)
 
-	// router (API) end-point Mockup 5
-	paymentRoute.GET("/transaction", controllerMgr.PaymentTransactionController.GetListPaymentTransaction)
-	paymentRoute.GET("/transactio/search", controllerMgr.PaymentTransactionController.GetPaymentTransactionById)
-	paymentRoute.POST("/transaction/create", controllerMgr.PaymentTransactionController.CreateNewPaymentTransaction)
-
-	paymentRoute.PUT("/transaction/update/:id", controllerMgr.PaymentTransactionController.UpdatePaymentTransaction)
-	paymentRoute.DELETE("/transaction/delete/:id", controllerMgr.PaymentTransactionController.DeletePaymentTransaction)
-
 	return router
 }
